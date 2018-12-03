0d0ebb8aa0dd1dc3966631e88c7f0c76bde61d7f
emilieaviles <43994902+emilieaviles@users.noreply.github.com>
2018-12-03 11:22:57 -0500

    1

---
+++ Lesson4/problem1/problem1.py
@@ -0,0 +1,7 @@
+score = 2.9
+
+result = (score / 4) * 100
+
+result = round(result)
+
+print(result)
---
+++ Lesson4/problem4/problem4.py
@@ -0,0 +1,15 @@
+length = 3
+width = 4
+height = 5
+
+Volume = length * width * height
+
+Volume = round(Volume)
+
+print(Volume)
+
+SurfaceArea = (2 * length * width) + (2 * height * length) + (2 * height * width)
+
+SurfaceArea = round(SurfaceArea)
+
+print(SurfaceArea)
